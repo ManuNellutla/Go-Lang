262ca2ce941ea9bdd83e16556fb7b0a0c861a298
manunellutla <manu.nellutla@gmail.com>
2019-08-28 08:11:30 -0400

    calculator commit

--- .idea/goFrist.go
+++ .idea/goFrist.go
@@ -3,8 +3,13 @@ package main
 import (
 	"fmt"
 	"math"
+	"time"
 )
 
 func main() {
 	fmt.Println("Hello, World", math.Phi)
+
+	fmt.Println("Welcome to the playground")
+	fmt.Println("The time is:", time.Now())
+
 }
